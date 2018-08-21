a641b0c65c2aa5c4ff3c27ce6f2d520f3f202374
spielchen <anna.bukavtsova@gmail.com>
2018-08-21 12:45:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ var mainList = {
 		for (var i = 0; i < 3; i++) {
 			var a = prompt("Какой тип товаров будем продавать?");
 			
-			if ((typeof(a)) === 'string' && (typeof(a)) !== null && a != '' && a.length < 50) {
+			if ((typeof(a)) === 'string' && a != '' && a.length < 50) {
 				console.log('Все верно!');
 				mainList.shopGoods[i] = a;
 			} else {
